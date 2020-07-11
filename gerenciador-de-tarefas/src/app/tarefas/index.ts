52c010a2d180bd93e82ffda2cfe8cdd73a18fc9a
Nei Borges Filho <naborgesfilho@gmail.com>
2020-07-11 15:44:06 -0300

    Adding Task Manager

---
+++ gerenciador-de-tarefas/src/app/tarefas/index.ts
@@ -0,0 +1,6 @@
+export * from './tarefas.module';
+export * from './shared/tarefa.model';
+export * from './listar';
+export * from './cadastrar';
+export * from './editar';
+export * from './tarefas-routing.module';
---
+++ gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { TarefaConcluidaDirective, TarefaService} from './shared';
+import { ListarTarefasComponent } from './listar';
+import { CadastrarTarefaComponent } from './cadastrar';
+import { EditarTarefaComponent } from './editar';
+
+
+@NgModule({
+  declarations: [ListarTarefasComponent, CadastrarTarefaComponent, EditarTarefaComponent, TarefaConcluidaDirective],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule
+  ],
+  providers : [
+    TarefaService
+  ]
+})
+export class TarefasModule { }
